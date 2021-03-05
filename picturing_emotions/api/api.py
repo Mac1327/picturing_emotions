9726633958742556be6ad0af6a5049954d7cfdef
Martin Clark <mac1327@googlemail.com>
2021-03-05 12:40:12 +0000

    started making api

---
+++ picturing_emotions/api/api.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from picturing_emotions import predict, MODEL_LOCATITON
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+# http://127.0.0.1:8000/predict_fare/?key=2012-10-06 12:10:20.0000001&pickup_datetime=2012-10-06 12:10:20 UTC&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2
+
+
+@app.get("/")
+def index():
+    return {"ok": "HELLO!"}
+
+@app.get("/predict_faces/")
+def predict_faces(list_of_arrays_of_images):
+    predictions = predict(list_of_arrays_of_images, MODEL_LOCATITON)
+    dict_of_predictions = {}
+    for face_number in len(range(predictions)):
+        dict_of_predictions[face_number] = predictions[face_number]
+    return dict_of_predictions
--- picturing_emotions/predict.py
+++ picturing_emotions/predict.py
@@ -12,9 +12,10 @@ CLASSES = ['surprise',
             "sadness",
             "happiness"]
 
+MODEL_LOCATITON = "raw_data/vg_face_model"
 
 def predict(list_of_arrays_of_images, model_location):
-    model = keras.models.load_model(model_location)
+    model = keras.models.load_model(MODEL_LOCATITON)
     list_of_predictions = []
     for image in list_of_arrays_of_images:
         prediction = model.predict(np.expand_dims(tf.image.resize(image, [224, 224]),axis=0)/255.0)
@@ -22,9 +23,14 @@ def predict(list_of_arrays_of_images, model_location):
     return list_of_predictions
 
 if __name__ == '__main__':
-    model_location = "raw_data/vg_face_model"
-    image1 = imageio.imread("raw_data/data_3000/train/anger/AF01ANS.png")
-    image2 = imageio.imread("raw_data/data_3000/train/fear/AF02AFS.png")
+    image1 = imageio.imread("raw_data/data_3000/train/disgust/17Exp1angry_actor_195.jpg")
+    image2 = imageio.imread("raw_data/data_3000/train/disgust/19Exp1angry_actor_198.jpg")
     list_of_arrays_of_images = [image1, image2]
-    predictions = predict(list_of_arrays_of_images, model_location)
-    print(f"predictions1 ={CLASSES[np.argmax(predictions[0])]}, predictions2 ={CLASSES[np.argmax(predictions[2])]}")
+
+    predictions = predict(list_of_arrays_of_images, MODEL_LOCATITON)
+ 
+    max_location = np.argmax(predictions[0])
+    print(f"predictions1 = {CLASSES[max_location]} : {round(predictions[0][max_location]*100)}%")
+    print("$$$$$$$$$$")
+    max_location = np.argmax(predictions[1])
+    print(f"predictions2 = {CLASSES[max_location]} : {round(predictions[1][max_location]*100)}%")
