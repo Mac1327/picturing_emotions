8e84e1220860c34d2e3cd15a5f5a8aaf3d843b12
karina pacut <k.pacut27@gmail.com>
2021-03-09 12:41:42 +0000

    cache added

--- app_combined.py
+++ app_combined.py
@@ -78,47 +78,61 @@ uploaded_file = st.file_uploader("Choose a photo")
 detector = MTCNN()
 
 if uploaded_file is not None:
-    image = plt.imread(uploaded_file)
-    image = cv2.resize(image, dsize=(1280, 720), interpolation=cv2.INTER_CUBIC)
-    image_origin = image.copy()
 
-    face_locations = detector.detect_faces(image)
+    @st.cache(suppress_st_warning=True, allow_output_mutation=True)
+    def upload():
 
-    pred_values = {}
-    X1, X2, Y1, Y2 = [],[],[],[]
+        image = plt.imread(uploaded_file)
+        image = cv2.resize(image, dsize=(1280, 720), interpolation=cv2.INTER_CUBIC)
+        image_origin = image.copy()
 
-    for i in range(len(face_locations)):
-        x1, y1, width, height =face_locations[i]["box"]
-        x2, y2 = x1 + width, y1 + height
+        face_locations = detector.detect_faces(image)
 
-        cropped = image[y1:y2, x1:x2]
-        pred = model.predict(np.expand_dims(resize((cropped), [224, 224]),axis=0)/255.0)[0]
-        top_values = pred.argsort()[-3:]
-        prediction1 = classes[top_values[2]]
-        pred1       = pred[top_values[2]]
-        prediction2 = classes[top_values[1]]
-        pred2       = pred[top_values[1]]
-        prediction3 = classes[top_values[0]]
-        pred3       = pred[top_values[0]]
-        
+        return image, image_origin, face_locations
 
-        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 5)
-        cv2.putText(image, f"1: {prediction1}:{round(pred1*100)}%", (x1, y2+30), 
-                                                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-        cv2.putText(image, f"2: {prediction2}:{round(pred2*100)}%", (x1, y2+60), 
-                                                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-        cv2.putText(image, f"3: {prediction3}:{round(pred3*100)}%", (x1, y2+90), 
-                                                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
 
-        pred_values[i] =  [[prediction1, round(pred1*100)],
-                           [prediction2, round(pred2*100)],
-                           [prediction3, round(pred3*100)]]
+    image, image_origin, face_locations = upload()
+    
+    
+    @st.cache(suppress_st_warning=True, allow_output_mutation=True)
+    def frame():
+        X1, X2, Y1, Y2 = [],[],[],[]
+        pred_values = {}
+        for i in range(len(face_locations)):
+            x1, y1, width, height =face_locations[i]["box"]
+            x2, y2 = x1 + width, y1 + height
 
-        X1.append(x1)
-        X2.append(x2)
-        Y1.append(y1)
-        Y2.append(y2)
+            cropped = image[y1:y2, x1:x2]
+            pred = model.predict(np.expand_dims(resize((cropped), [224, 224]),axis=0)/255.0)[0]
+            top_values = pred.argsort()[-3:]
+            prediction1 = classes[top_values[2]]
+            pred1       = pred[top_values[2]]
+            prediction2 = classes[top_values[1]]
+            pred2       = pred[top_values[1]]
+            prediction3 = classes[top_values[0]]
+            pred3       = pred[top_values[0]]
+            
 
+            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 5)
+            cv2.putText(image, f"1: {prediction1}:{round(pred1*100)}%", (x1, y2+30), 
+                                                        cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+            cv2.putText(image, f"2: {prediction2}:{round(pred2*100)}%", (x1, y2+60), 
+                                                        cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+            cv2.putText(image, f"3: {prediction3}:{round(pred3*100)}%", (x1, y2+90), 
+                                                        cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+
+            pred_values[i] =  [[prediction1, round(pred1*100)],
+                            [prediction2, round(pred2*100)],
+                            [prediction3, round(pred3*100)]]
+
+            X1.append(x1)
+            X2.append(x2)
+            Y1.append(y1)
+            Y2.append(y2)
+
+        return image, X1, X2, Y1, Y2, pred_values
+
+    image, X1, X2, Y1, Y2, pred_values = frame()
     fig, ax = plt.subplots(figsize=(7, 3))
     plt.axis('off') 
     ax.imshow(image)
@@ -126,24 +140,41 @@ if uploaded_file is not None:
     #st.write(pred)
 
 
+    @st.cache(suppress_st_warning=True, allow_output_mutation=True)
+    def pick_img():
+        to_pick = []  
 
-    to_pick = []  
-    for i in range(len(face_locations)):
-        x1, x2, y1, y2 = X1[i], X2[i], Y1[i], Y2[i]
-        to_pick.append(image_origin[y1:y2, x1:x2])
+        for i in range(len(face_locations)):
+            x1, x2, y1, y2 = X1[i], X2[i], Y1[i], Y2[i]
+            to_pick.append(image_origin[y1:y2, x1:x2])
+        return to_pick
+
+    to_pick = pick_img()
+
+    @st.cache(allow_output_mutation=True)
+    def get_mutable():
+        return {}
 
     img_num = st.slider("Which image?", 1, len(to_pick))
-  
+    mutable_object = get_mutable()
+    mutable_object[img_num] = [to_pick[img_num -1], pred_values[img_num -1][0][0],  pred_values[img_num -1][0][1],
+    pred_values[img_num -1][1][0], pred_values[img_num -1][1][1], pred_values[img_num -1][2][0],
+    pred_values[img_num -1][2][1]]
+
         
     if img_num:
-        st.image(to_pick[img_num -1])
-        st.write('1:', pred_values[img_num -1][0][0],':',  pred_values[img_num -1][0][1],'%') 
-        st.write('2:', pred_values[img_num -1][1][0],':',  pred_values[img_num -1][1][1],'%')
-        st.write('3:', pred_values[img_num -1][2][0],':',  pred_values[img_num -1][2][1],'%') 
+     
+        st.image(mutable_object[img_num][0])
+        #st.image(to_pick[img_num -1])
+        #st.write('1:', pred_values[img_num -1][0][0],':',  pred_values[img_num -1][0][1],'%') 
+        #st.write('2:', pred_values[img_num -1][1][0],':',  pred_values[img_num -1][1][1],'%')
+        #st.write('3:', pred_values[img_num -1][2][0],':',  pred_values[img_num -1][2][1],'%') 
 
-
+        st.write('1:', mutable_object[img_num][1],':', mutable_object[img_num][2],'%') 
+        st.write('2:',  mutable_object[img_num][3],':', mutable_object[img_num][4],'%')
+        st.write('3:',  mutable_object[img_num][5],':', mutable_object[img_num][6],'%')   
     
-
+       
 
 
 HTML3 = f"""
